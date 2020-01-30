9472e91d7dce7ddfa18564473ab99ecb1217e012
Denis Simon <denis.v.simon@gmail.com>
2020-01-31 01:01:58 +0300

    Update SwiftEvents.h

--- Sources/SwiftEvents.h
+++ Sources/SwiftEvents.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for SwiftEvents.
-FOUNDATION_EXPORT double SwiftEvents_VersionNumber;
+FOUNDATION_EXPORT double SwiftEventsVersionNumber;
 
 //! Project version string for SwiftEvents.
-FOUNDATION_EXPORT const unsigned char SwiftEvents_VersionString[];
+FOUNDATION_EXPORT const unsigned char SwiftEventsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftEvents/PublicHeader.h>
 
